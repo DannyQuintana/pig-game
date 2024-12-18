01ed417ca3b7406531802d901c6ca377bd09b3ca
DannyQuintana <daniel.j.quintana3391@gmail.com>
2024-12-18 00:55:43 -0800

    feat: initialize application with basic functionality
    - Created variables to select DOM elements.
    - Set up the initial application state.
    - Added a function to handle dice rolling.

--- script.js
+++ script.js
@@ -1,1 +1,25 @@
 'use strict';
+
+// Selecting elements
+const score0El = document.getElementById('score--0');
+const score1El = document.getElementById('score--1');
+const diceEl = document.querySelector('.dice');
+const btnNew = document.querySelector('.btn--new');
+const btnRoll = document.querySelector('.btn--roll');
+const btnHold = document.querySelector('.btn--hold');
+
+// New Game
+score0El.textContent = 0;
+score1El.textContent = 0;
+diceEl.classList.add('hidden');
+
+btnRoll.addEventListener('click', () => {
+  // Generate random dice roll
+  const dice = Math.trunc(Math.random() * 6) + 1;
+  // display dice
+  console.log(dice);
+  diceEl.classList.remove('hidden');
+  diceEl.src = `dice-${dice}.png`;
+
+  // check if roll is 1 if true switch else add to score
+});
